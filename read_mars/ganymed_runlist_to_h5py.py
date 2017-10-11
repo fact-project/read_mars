276d1e97d4c6074cc1ff2eeafd8acd586fd5bcbb
Dominik Neise <neised@phys.ethz.ch>
2017-10-11 09:11:48 +0200

    use fact.path instead of direct string manipulation

--- read_mars/ganymed_runlist_to_h5py.py
+++ read_mars/ganymed_runlist_to_h5py.py
@@ -8,11 +8,12 @@ Arguments:
 '''
 from argparse import ArgumentParser
 import pandas as pd
+import fact
+from functools import partial
 from fact.io import to_h5py
 from tqdm import tqdm
-import os
 
-from . import read_mars, datepath
+from . import read_mars
 
 
 parser = ArgumentParser(description=__doc__)
@@ -29,17 +30,19 @@ def main():
     runs = pd.read_csv(args.runlist)
     runs['night_date'] = pd.to_datetime(runs['night'].astype(str), format='%Y%m%d')
 
+    ganymed_file_path_generator = partial(
+        fact.path.tree_path,
+        prefix=args.ganymed_base,
+        suffix='-summary.root',
+    )
+
     initialised = False
     for idx, run in tqdm(runs.iterrows(), total=len(runs)):
-        night = int('{:%Y%m%d}'.format(run.night_date))
-        base = datepath(args.ganymed_base, run.night_date)
-
-        ganymed_file = os.path.join(
-            base,
-            '{}_{:03d}-summary.root'.format(night, run.run_id)
+        ganymed_file_path = ganymed_file_path_generator(
+            night=int('{:%Y%m%d}'.format(run.night_date)),
+            run=int(run.run_id)
         )
-
-        df = read_mars(ganymed_file, tree='Events')
+        df = read_mars(ganymed_file_path, tree='Events')
         df['night'] = night
         df['run_id'] = run.run_id
 
