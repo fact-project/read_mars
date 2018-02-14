8593707905a8acea3ad409ab8e4d560e843bc104
Jens Buss <jens.buss@tu-dortmund.de>
2018-02-14 15:55:26 +0100

    bump version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='read_mars',
-    version='0.0.5',
+    version='0.0.6',
     author='Maximilian Nöthe',
     author_email='maximilian.noethe@tu-dortmund.de',
     packages=['read_mars'],
